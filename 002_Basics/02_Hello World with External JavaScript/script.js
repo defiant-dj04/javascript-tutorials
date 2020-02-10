374e646a253ca7facc872d571608e1e6b6fae6cf
Deepjyoti Barman <deepjyotibarman@zoho.com>
2020-02-10 08:56:20 +0530

    v0.2 changes Added: .gitignore Added: 002_Basics/01_Hello World with Internal Javascript/index.html Added: 002_Basics/02_Hello World with External JavaScript/index.html Added: 002_Basics/02_Hello World with External JavaScript/script.js

---
+++ 002_Basics/02_Hello World with External JavaScript/script.js
@@ -0,0 +1,7 @@
+// document is the by default object given in JavaScript that represents the entire document
+// document object is used to call the write() function to print the message
+document.write("<h1>Hello world</h1>");
+
+// To check logged messages, open 'Developer Console' in browser
+// Google Chrome: F12 / Ctrl + Shift + J (Shortcut)
+console.log("How are you all?");
